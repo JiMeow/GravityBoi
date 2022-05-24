328df00410756753e6f05fa7c20ac3bdebcbefb7
jiratQ <jirat123@icloud.com>
2022-05-24 22:42:09 +0700

    update debug.Log

--- Scripts/InvisibleWall.cs
+++ Scripts/InvisibleWall.cs
@@ -7,7 +7,6 @@ public class InvisibleWall : MonoBehaviour
     void Update()
     {
         Transform wallTransfrom = GetComponent<Transform>();
-        Debug.Log(wallTransfrom.position.x);
         if (wallTransfrom.position.x < -25)
         {
             wallTransfrom.position = new Vector3(-30, wallTransfrom.position.y, wallTransfrom.position.z);
